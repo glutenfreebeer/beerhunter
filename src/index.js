bed4ec116ce5585527ee6ba8bd51c467f126c1dd
Andrea Stagi <stagi.andrea@gmail.com>
2018-12-27 20:02:08 +0100

    Add Axios requests

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 const cron = require("node-cron");
 const express = require("express");
 const fs = require("fs");
+const axios = require('axios');
 
 
 app = express();
 
 cron.schedule("* * * * *", function () {
-    console.log("running a task every minute");
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+        .then(response => {
+            console.log(response.data.url);
+            console.log(response.data.explanation);
+        })
+        .catch(error => {
+            console.log(error);
+        });
 });
 
 app.listen(3128);
